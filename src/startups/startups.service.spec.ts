18a2b1d9ba49b35c4bf02c63eeb9d5e97b8188e9
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-02 14:32:39 +0200

    *edit* back / startup service : testing module now deals with startups injection + findOne method tested

--- src/startups/startups.service.spec.ts
+++ src/startups/startups.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StartupsService } from './startups.service';
+import { mockStartups } from '../mocks/mockStartups';
 
 describe('StartupService', () => {
   let service: StartupsService;
 
   beforeEach(async () => {
+    // jest.mock('../constants/startups', () => mockStartups);
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StartupsService],
+      providers: [
+        StartupsService,
+        { provide: 'STARTUPS_DATA', useValue: mockStartups },
+      ],
     }).compile();
 
     service = module.get<StartupsService>(StartupsService);
@@ -15,4 +20,8 @@ describe('StartupService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be mockstartup 1', () => {
+    expect(service.findOne(1)).toBe(mockStartups[0])
+  })
 });
