5f9129b53e3b867ee21fa47efeb72c1bf04f4d65
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-02 14:33:26 +0200

    *edit* back / startup controller : testing module now deals with startups injection + getStartup method testing

--- src/startups/startups.controller.spec.ts
+++ src/startups/startups.controller.spec.ts
@@ -1,19 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StartupsController } from './startups.controller';
+import { StartupsService } from './startups.service';
+import { mockStartups } from '../mocks/mockStartups';
 
 describe('StartupsController', () => {
-  let controller: StartupsController;
+    let startupsController: StartupsController;
+    let startupsService : StartupsService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [StartupsController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+        controllers: [StartupsController],
+        providers: [
+            StartupsService,
+            { provide: 'STARTUPS_DATA', useValue: mockStartups },
+        ],
+        }).compile();
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    controller = module.get<StartupsController>(StartupsController);
-  });
+        startupsService = module.get<StartupsService>(StartupsService);
+        startupsController = module.get<StartupsController>(StartupsController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(startupsController).toBeDefined();
+    });
+
+    it('it should call startupsService.findOne and get MockData Inc. data', () => {
+        const spiedOnService = jest.spyOn(startupsService, 'findOne').mockImplementation(() => mockStartups[0])
+        expect(spiedOnService).toHaveBeenCalledTimes(0)
+        expect(startupsController.getStartup("0")).toBe(mockStartups[0])
+        expect(spiedOnService).toHaveBeenCalledTimes(1)
+    })
 });
