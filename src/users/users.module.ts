a7550852275b8b50cadc13e9f2486ba4a1b93eb8
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-02 14:31:47 +0200

    *edit* back / testing module now deals with users injection + findOne method testing

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import users from '../constants/users';
 
 @Module({
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+        provide: 'USERS_DATA',
+        useValue: users,
+    },
+  ],
   exports: [UsersService]
 })
 export class UsersModule {}
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import mockUsers from '../mocks/mockUsers';
 
 describe('UsersService', () => {
-  let service: UsersService;
+    let service: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                UsersService,
+                { provide: 'USERS_DATA', useValue: mockUsers }, // [!] injecting mock data
+            ],
+        }).compile();
 
-    service = module.get<UsersService>(UsersService);
-  });
+        service = module.get<UsersService>(UsersService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('should be retrieve the expected user when findone is called with an existing username', () => {
+        expect(service.findOne("user1")?.userId).toBe(mockUsers[0].userId)
+        expect(service.findOne("user2")?.userId).toBe(mockUsers[1].userId)
+        /*expect(service.findOne("john")?.username).toBe("john")
+        expect(service.findOne("maria")?.username).toBe("maria")*/
+    })
 });
