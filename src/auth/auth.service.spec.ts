5a9197988190e6cbf4166f9984e0ec0ffed2c32a
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-03 02:24:12 +0200

    *edit* tests / auth service : test access token returned when signing in

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,27 +3,32 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './jwtConstants';
+import users from '../constants/users';
 
 describe('AuthService', () => {
-  let service: AuthService;
+    let service: AuthService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        UsersModule,
-        JwtModule.register({
-          global: true,
-          secret: jwtConstants.secret,
-          signOptions: { expiresIn: '60s' },
-        }),
-      ],
-      providers: [AuthService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                UsersModule,
+                JwtModule.register({
+                    global: true,
+                    secret: jwtConstants.secret,
+                    signOptions: { expiresIn: '60s' },
+                }),
+            ],
+            providers: [AuthService],
+        }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
+        service = module.get<AuthService>(AuthService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('Sign In', async () => {
+        expect((await service.signIn(users[0].username, "changeme")).access_token).toBeDefined()
+    })
 });
