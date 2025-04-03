de74d78ae93af2131f38e0e3b3457e47b7c625ab
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-03 02:30:25 +0200

    *edit* tests / auth service : sign in return an access token with the expected values

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,6 +4,7 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './jwtConstants';
 import users from '../constants/users';
+import { jwtDecode } from 'jwt-decode';
 
 describe('AuthService', () => {
     let service: AuthService;
@@ -28,7 +29,12 @@ describe('AuthService', () => {
         expect(service).toBeDefined();
     });
 
-    it('Sign In', async () => {
+    it('Sign In return an access token with the expected values', async () => {
         expect((await service.signIn(users[0].username, "changeme")).access_token).toBeDefined()
+        const token = (await service.signIn(users[0].username, "changeme")).access_token
+        const decodedToken = jwtDecode<{admin : boolean, username : string, sub : number}>(token)
+        expect(decodedToken.admin).toBeFalsy()
+        expect(decodedToken.username).toBe("john")
+        expect(decodedToken.sub).toBe(1)
     })
 });
